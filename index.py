09e8fbddb106b370e01e0a41f486866d48affe31
Ameer Ayoub <ameer.ayoub@gmail.com>
2011-08-27 13:59:57 -0700

    pulled settings into separate file

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from tosql import DatabaseTypes, Database, Options, executescript, get_schema_prefix
+from tosql import DatabaseTypes, executescript, get_schema_prefix
+from settings import Database, Options
 
 def mk_index(name):
 	return "%s/%s.index.sql" % (Options.schema_dir, name)
@@ -7,6 +8,6 @@ def create_indices(cursor):
 	executescript(open(mk_index(get_schema_prefix(Database.type))), debug = True)
 
 if __name__ == "__main__":
-		
+	conn, cursor = connect_db(Database)
 	create_indices(cursor)
 
